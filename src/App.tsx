38e76a99f247eb3e8885d6164b7def37c224a354
Steve Ernstberger <steven.ernstberger@pinnacol.com>
2024-02-09 12:47:20 -0700

    added vr

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,27 @@ import Ball from "./components/Ball";
 import PoolTable from "./components/PoolTable";
 import Background from "./components/Background";
 const exrUrl = new URL("../public/country_club_4k.exr", import.meta.url).href;
+import { VRButton, ARButton, XR, Controllers, Hands } from "@react-three/xr";
 
 const App = () => {
   return (
-    <Canvas style={{ height: "100vh", width: "100vw" }}>
-      <Suspense fallback={null}>
-        <PerspectiveCamera makeDefault position={[0, 5, 10]} />
-        <ambientLight intensity={1} />
-        {/* <pointLight position={[10, 10, 10]} intensity={20} /> */}
-        <spotLight position={[0, 10, 0]} intensity={30} />
-        <Ball position={[0, 1, 0]} />
-        <PoolTable />
-        <Background exrUrl={exrUrl} />
-        <OrbitControls />
-      </Suspense>
-    </Canvas>
+    <>
+      <VRButton />
+
+      <Canvas style={{ height: "100vh", width: "100vw" }}>
+        <XR>
+          <Suspense fallback={null}>
+            <PerspectiveCamera makeDefault position={[0, 5, 10]} />
+            <ambientLight intensity={1} />
+            <spotLight position={[0, 10, 0]} intensity={30} />
+            <Ball position={[0, 1, 0]} />
+            <PoolTable />
+            <Background exrUrl={exrUrl} />
+            <OrbitControls />
+          </Suspense>
+        </XR>
+      </Canvas>
+    </>
   );
 };
 
